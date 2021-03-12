97332313d7afda23e82cb9d4db3aaa3bcc788ce4
briws <briws@pm.me>
2021-03-12 18:04:23 +0300

    lesson 01 + homework complete

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function hello() {
+  debugger;
+  alert('Hello Briws');
+}
+
+// hello();
+
 function App() {
+  // полезное что-то
+  // обязана вернуть JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is APP component.
+      <Rating />
+      <Accordion />
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  )
+}
+
+function Star() {
+  return (
+    <div>
+      <div>* Star *</div>
+    </div>
+  );
+}
+
+function Accordion() {
+  return <div>
+    <h3>Меню</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
 }
 
 export default App;
