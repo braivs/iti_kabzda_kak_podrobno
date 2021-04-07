f66a3efbefc02349e597ca9827da586797501dea
briws <briws@pm.me>
2021-04-08 00:08:22 +0300

    06 Homework 1/2 - complete

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState} from 'react';
 
 type AccordionPropsType = {
   titleValue: string
 }
 
 function UncontrolledAccordion(props: AccordionPropsType) {
-  console.log("UncontrolledAccordion rendering")
+  console.log('UncontrolledAccordion rendering')
 
-  const collapsed = false;
+  // const collapsed = false;
+  let [collapsed, collapsedOn] = useState(false);
 
-    return <div>
-      <AccordionTitle title={props.titleValue}/> <button>TOGGLE</button>
-      { !collapsed && <AccordionBody/> }
-    </div>
+  const toggleOn = () => {
+    !collapsed ? collapsedOn(true) : collapsedOn(false)
+  }
+
+  return <div>
+    <AccordionTitle title={props.titleValue}/>
+    <button onClick={() => toggleOn()}>TOGGLE</button>
+    {/*<AccordionTitle title={props.titleValue}/> <button onClick={ () => collapsedOn}>TOGGLE</button>*/}
+    {!collapsed && <AccordionBody/>}
+  </div>
 
 }
 
@@ -21,12 +28,10 @@ type AccordionTitlePropsType = {
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-  console.log("AccordionTitle rendering")
   return <h3>--{props.title}--</h3>
 }
 
 function AccordionBody() {
-  console.log("AccordionBody rendering")
   return <ul>
     <li>1 gl</li>
     <li>2 hf</li>
