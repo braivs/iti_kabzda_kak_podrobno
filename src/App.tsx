cd06127aa916633f96ae0756cad40ef3404c7e86
briws <briws@pm.me>
2021-04-13 17:29:58 +0300

    08 homework quest 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React, {useState} from 'react';
 import './App.css';
 import {Rating, RatingValueType} from './components/Rating/Rating';
 import {UncontrolledRating} from './components/UncontrolledRating/UncontrolledRating';
+import Accordion from './components/Accordion/Accordion';
 
 function App(props: any) {
   console.log('App rendering')
 
-  let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+  let [ratingValue, setRatingValue] = useState<RatingValueType>(4)
+  let [accordionCollapsed, setAccordianCollapsed] = useState<boolean>(true)
+
+
   return (
     <div className={'App'}>
 
@@ -14,6 +18,8 @@ function App(props: any) {
               onClick={setRatingValue}/>
 
       <UncontrolledRating />
+      
+      <Accordion titleValue={'Menu'} collapsed={accordionCollapsed} />
 
     </div>
   );
