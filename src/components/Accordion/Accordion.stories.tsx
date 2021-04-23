20667a27bbe82f86f2179c3d565150c625de460b
briws <briws@pm.me>
2021-04-24 02:04:08 +0300

    11.2 progress 22:40 min

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {Accordion} from './Accordion';
+import {Accordion, AccordionPropsType} from './Accordion';
 import {action} from '@storybook/addon-actions';
+import {Story} from '@storybook/react';
 
 
 export default {
@@ -10,8 +11,21 @@ export default {
 
 const callback = action('accordion mode change event fired');
 
-export const MenuCollapsedMode = () => <Accordion titleValue={'Menu'} collapsed={true} onChange={callback} />;
-export const UsersUncollapsedMode = () => <Accordion titleValue={'Users'} collapsed={false} onChange={callback} />;
+const Template: Story<AccordionPropsType> = (args) => <Accordion {...args} />;
+
+export const MenuCollapsedMode = Template.bind({})
+MenuCollapsedMode.args = {
+  titleValue: 'Menu',
+  collapsed: true,
+  onChange: callback
+}
+
+export const UsersUncollapsedMode = Template.bind({})
+UsersUncollapsedMode.args = {
+  titleValue: 'Users',
+  collapsed: false,
+  onChange: callback
+}
 
 export const ModeChanging = () => {
   const [value, setValue] = useState<boolean>(true)
