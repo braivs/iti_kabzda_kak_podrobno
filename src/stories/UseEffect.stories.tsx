ad5e53598fb12ceb9c03c4cbe4a061c91dbd8eb2
BriWS <brightwiths@gmail.com>
2021-07-26 19:40:38 +0300

    21 complete (without homework)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -39,19 +39,19 @@ export const SetTimeoutExample = () => {
 
   console.log('SetTimeoutExample');
 
-  /*useEffect(() => {
-    console.log('useEffect every render')
-    document.title = counter.toString();
-  })*/
+  useEffect(() => {
 
-  setTimeout(() => {
-    debugger;
-    document.title = counter.toString();
-  }, 1000)
+    setInterval(() => {
+      setCounter((state) => state +1)
+    }, 1000)
+
+  },[])
+
+
 
   return <>
-    Hello, {counter} {fake}
-    <button onClick={() => setFake(fake + 1)}>fake+</button>
-    <button onClick={() => setCounter(counter + 1)}>counter+</button>
+    Hello, counter: {counter} - fake: {fake}
+    {/*<button onClick={() => setFake(fake + 1)}>fake+</button>
+    <button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
   </>
 }
