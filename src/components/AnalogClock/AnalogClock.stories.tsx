4cc8baa9e5d12bd50b944bbbd8b25182d0e54d06
BriWS <brightwiths@gmail.com>
2021-07-28 15:32:44 +0300

    22 homework complete

---
+++ src/components/AnalogClock/AnalogClock.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AnalogClock} from "./AnalogClock";
+
+export default {
+  title: 'AnalogClock',
+  component: AnalogClock,
+
+}
+
+export const BaseExample = () => {
+  return <AnalogClock />
+}
